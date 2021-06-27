38456013c3d566edf5c74b2cf9b59acee64764df
Ezgi Aydın <83668857+ezgiaydin@users.noreply.github.com>
2021-06-27 15:31:52 +0300

    Add files via upload

---
+++ CustomerManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class CustomerManager
+    {
+        public void Add(Customer musteri)
+        {
+            Console.WriteLine(musteri.customerId + " " + musteri.customerName + " " + musteri.customerSurname);
+        }
+        public void List(Customer liste)
+        {
+            Console.WriteLine(liste.customerId + " " + liste.customerName + " " + liste.customerSurname);
+        }
+        public void Delete(Customer sil)
+        {
+            Console.WriteLine(sil.customerId + " " + sil.customerName + " " + sil.customerSurname);
+        }
+    }
+}
+
+
---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer c1 = new Customer();
+            c1.customerId = 12345;
+            c1.customerName = "ezgi";
+            c1.customerSurname = "aydın";
+
+
+            Customer c2 = new Customer();
+            c2.customerId = 34567;
+            c2.customerName = "deniz";
+            c2.customerSurname = "aküzüm";
+
+
+            Customer[] musteriler = new Customer[] { c1, c2 };
+            foreach (var i in musteriler)
+            {
+                Console.WriteLine(i.customerId + "-" + i.customerName+ "" + i.customerSurname);
+            }
+
+            CustomerManager customerManager = new CustomerManager();
+            Console.WriteLine("-----add costumer(s)----");
+            customerManager.Add(c2);
+            Console.WriteLine("----list of customers----");
+            customerManager.List(c2);
+            customerManager.List(c1);
+            Console.WriteLine("----delete customer(s)----");
+            customerManager.Delete(c1);
+
+        }
+    }
+}
